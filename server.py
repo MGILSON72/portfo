0044a2cc63bd4ceea6fe1d3345abe8a962566355
mgilsonMT <mgilson@meditech.com>
2023-06-22 15:10:29 -0400

    Update script method documentation

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
+from __future__ import annotations
 from flask import Flask, render_template, request, redirect
+from typing import Union, TYPE_CHECKING
 import csv
+
+if TYPE_CHECKING:
+    from werkzeug.wrappers import Response as BaseResponse
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def my_home():
+def my_home() -> str:
+    """Renders the home page template."""
     return render_template('index.html')
 
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        name = data['name']
-        email = data['email']
-        message = data['message']
-        file = database.write(f'\n{name},{email},{message}')
+def write_to_csv(data: dict[str, str]) -> None:
+    """Writes the data dictionary passed into a database.csv file.
 
-
-def write_to_csv(data):
+    Args:
+        data - dictionary of values from contact form
+            {'name': 'John Doe','email': 'jdoe@gmail.com','message': 'This is a message'}
+    """
     with open('database.csv', newline='', mode='a') as database2:
         name = data['name']
         email = data['email']
@@ -27,7 +31,13 @@ def write_to_csv(data):
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
-def submit_form():
+def submit_form() -> Union[BaseResponse, str]:
+    """Submits the form data to be stored into a csv file.
+
+    Returns:
+        A BaseResponse if the form data was successfully recorded otherwise a message is presented
+        to the user.
+    """
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
